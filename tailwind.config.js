1a15210ad3378c3b90790881de7b53ca9bfa6486
Flavio Vieira Vissoto <vissoto_flavio@hotmail.com>
2024-04-01 11:55:11 -0300

    Melhoria nas resoluções

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,27 @@
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        sm: '100%',
+        md: '768px',
+        lg: '1024px',
+        '2lg': '1280px',
+        xl: '1366px',
+        '2xl': '1440px',
+      },
+    },
     screens: {
-      sm: '320px',
-      sl: '640px',
-      tb: '768px',
+      sm: '390px',
+      '2sm': '600px',
+      md: '768px',
       lg: '1024px',
-      xl: '1280px',
+      '2lg': '1280px',
+      xl: '1366px',
       '2xl': '1440px',
-      '3xl': '1920px',
+      '3xl': '1536px',
+      '4xl': '1920px',
     },
     extend: {},
   },
